454e365286cbd727c78449698bf995d6e8c6a164
Kris McAulay <kris@mcaulay.ca>
2016-06-16 05:43:37 -0300

    Updates.

--- examples/FindTmxParser.cmake
+++ examples/FindTmxParser.cmake
@@ -3,24 +3,29 @@
 # See http://opensource.org/licenses/MIT
 
 # TMXPARSER_INCLUDE_DIR
-# TMXPARSER_LIBRARY
+# TMXPARSER_LIBRARY_DEBUG
+# TMXPARSER_LIBRARY_RELEASE
 # TMXPARSER_FOUND
 
 find_path(TMXPARSER_INCLUDE_DIR NAMES tmxparser/Tmx.h
           DOC "The tmxparser include directory"
 )
 
-find_library(TMXPARSER_LIBRARY NAMES tmxparser
+find_library(TMXPARSER_LIBRARY_DEBUG NAMES tmxparser
           DOC "The tmxparser library"
 )
 
-if(TMXPARSER_LIBRARY)
+find_library(TMXPARSER_LIBRARY_RELEASE NAMES tmxparser
+          DOC "The tmxparser library"
+)
+
+if(TMXPARSER_LIBRARY_DEBUG AND TMXPARSER_LIBRARY_RELEASE)
   set(TMXPARSER_FOUND TRUE)
 else()
   set(TMXPARSER_FOUND FALSE)
 endif()
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(TMXPARSER DEFAULT_MSG TMXPARSER_LIBRARY TMXPARSER_INCLUDE_DIR)
+find_package_handle_standard_args(TMXPARSER DEFAULT_MSG TMXPARSER_LIBRARY_DEBUG TMXPARSER_LIBRARY_RELEASE TMXPARSER_INCLUDE_DIR)
 
 mark_as_advanced(TMXPARSER_INCLUDE_DIR TMXPARSER_LIBRARY)
